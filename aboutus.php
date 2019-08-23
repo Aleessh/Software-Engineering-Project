389db47bf6a09e746a248ec32bdaef9c4446a47e
aleessh <alishkayastha73@gmail.com>
2019-08-23 21:31:38 +0545

    first commit

---
+++ aboutus.php
@@ -0,0 +1,66 @@
+<?php session_start();?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<?php
+			include("includes/head.inc.php");
+		?>
+</head>
+
+<body>
+			<!-- start header -->
+				<div id="header">
+					<div id="menu">
+						<?php
+							include("includes/menu.inc.php");
+						?>
+					</div>
+				</div>
+				
+				<div id="logo-wrap">
+					<div id="logo">
+							<?php
+								include("includes/logo.inc.php");
+							?>
+					</div>
+				</div>
+			<!-- end header -->
+			
+			<!-- start page -->
+
+				<div id="page">
+					<!-- start content -->
+					<div id="content">
+						<div class="post">
+							<h1 class="title">About Us</h1>
+							<div class="entry" style="height:auto">
+								The main objective of the project is to create an online book store that allows users to search and purchase a book based on title, author and subject. The selected books are displayed in a tabular format and the user can order their books online through credit card payment. The Administrator will have additional functionalities when compared to the common user.   
+							</div>
+							
+						</div>
+						
+					</div>
+					<!-- end content -->
+					
+					<!-- start sidebar -->
+					<div id="sidebar">
+							<?php
+								include("includes/search.inc.php");
+							?>
+					</div>
+					<!-- end sidebar -->
+					<div style="clear: both;">&nbsp;</div>
+				</div>
+			<!-- end page -->
+			
+			<!-- start footer -->
+				<div id="footer">
+							<?php
+								include("includes/footer.inc.php");
+							?>
+				</div>
+			<!-- end footer -->
+</body>
+</html>
